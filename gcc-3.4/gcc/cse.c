e89cce1ccb12269469246881f025b77b3a04fabd
Bernd Schmidt <bernds_cb1@t-online.de>
2004-10-18 10:52:16 +0000

    Don't CSE argument area save/restore into a no-op instruction
    git-svn-id: svn://localhost/svn/toolchain/trunk@183 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-3.4/gcc/cse.c
+++ gcc-3.4/gcc/cse.c
@@ -5761,12 +5761,7 @@ cse_insn (rtx insn, rtx libcall_insn)
 	  if (GET_CODE (dest) == REG || GET_CODE (dest) == SUBREG)
 	    invalidate (dest, VOIDmode);
 	  else if (GET_CODE (dest) == MEM)
-	    {
-	      /* Outgoing arguments for a libcall don't
-		 affect any recorded expressions.  */
-	      if (! libcall_insn || insn == libcall_insn)
-		invalidate (dest, VOIDmode);
-	    }
+	    invalidate (dest, VOIDmode);
 	  else if (GET_CODE (dest) == STRICT_LOW_PART
 		   || GET_CODE (dest) == ZERO_EXTRACT)
 	    invalidate (XEXP (dest, 0), GET_MODE (dest));
@@ -5934,12 +5929,7 @@ cse_insn (rtx insn, rtx libcall_insn)
 	if (GET_CODE (dest) == REG || GET_CODE (dest) == SUBREG)
 	  invalidate (dest, VOIDmode);
 	else if (GET_CODE (dest) == MEM)
-	  {
-	    /* Outgoing arguments for a libcall don't
-	       affect any recorded expressions.  */
-	    if (! libcall_insn || insn == libcall_insn)
-	      invalidate (dest, VOIDmode);
-	  }
+	  invalidate (dest, VOIDmode);
 	else if (GET_CODE (dest) == STRICT_LOW_PART
 		 || GET_CODE (dest) == ZERO_EXTRACT)
 	  invalidate (XEXP (dest, 0), GET_MODE (dest));
