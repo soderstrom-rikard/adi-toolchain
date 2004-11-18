7256617c66aa23fe37451a40eee0f4d709062e4e
Bernd Schmidt <bernds_cb1@t-online.de>
2004-11-18 20:43:04 +0000

    init/fini support
    git-svn-id: svn://localhost/svn/toolchain/trunk@264 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-3.4/gcc/config/bfin/elf.h
+++ gcc-3.4/gcc/config/bfin/elf.h
@@ -12,3 +12,9 @@
 #define ASM_GENERATE_INTERNAL_LABEL(LABEL, PREFIX, NUM)		\
      sprintf (LABEL, "*%s%s$%d", LOCAL_LABEL_PREFIX, PREFIX, (int) NUM)
 
+/* Now we define the strings used to build the spec file.  */
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC	"crt0%O%s crti%O%s crtbegin%O%s"
+
+#undef  ENDFILE_SPEC
+#define ENDFILE_SPEC	"crtend%O%s crtn%O%s"
