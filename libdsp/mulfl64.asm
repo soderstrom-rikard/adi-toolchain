9f64f62cc5af99239b9b387fbadda75ea433437f
Jie Zhang <jie.zhang@analog.com>
2006-03-31 16:26:25 +0000

    [T342] Change license to GNU GPL + libgcc exception.
    git-svn-id: svn://localhost/svn/toolchain/trunk@911 2adf4935-75e9-4407-93b2-34b21a1880a4

--- libdsp/mulfl64.asm
+++ libdsp/mulfl64.asm
@@ -1,11 +1,20 @@
 /*
 ** Copyright (C) 2003-2006 Analog Devices, Inc.
 **
-** This file is subject to the terms and conditions of the GNU Library General  
-** Public License. See the file "COPYING.LIB" in the main directory of this  
-** archive for more details.  
+** This file is subject to the terms and conditions of the GNU General  
+** Public License. See the file COPYING in the main directory of this  
+** archive for more details.
 **
-** Non-LGPL License also available as part of VisualDSP++  
+** In addition to the permissions in the GNU General Public License,
+** Analog Devices gives you unlimited permission to link the
+** compiled version of this file into combinations with other programs,
+** and to distribute those combinations without any restriction coming
+** from the use of this file.  (The General Public License restrictions
+** do apply in other respects; for example, they cover modification of
+** the file, and distribution when not linked into a combine
+** executable.)  
+**
+** Non-GPL License also available as part of VisualDSP++  
 ** http://www.analog.com/processors/resources/crosscore/visualDspDevSoftware.html
 **
 ** 64-bit IEEE floating point multiplication.
