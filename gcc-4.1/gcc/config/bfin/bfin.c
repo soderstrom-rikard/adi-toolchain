d39d7ed3d22f8e88c6494636c728d2db9323ceb4
Jie Zhang <jie.zhang@analog.com>
2006-06-13 07:32:38 +0000

    [T220]	* config/bfin/bfin.c (bfin_reorg_loops): Only call recog_memoized () 	for real instruction.
    git-svn-id: svn://localhost/svn/toolchain/trunk@1032 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/config/bfin/bfin.c
+++ gcc-4.1/gcc/config/bfin/bfin.c
@@ -3442,7 +3442,7 @@ bfin_reorg_loops (FILE *dump_file)
 	tail = PREV_INSN (tail);
 
       bb->aux = NULL;
-      if (recog_memoized (tail) == CODE_FOR_loop_end)
+      if (INSN_P (tail) && recog_memoized (tail) == CODE_FOR_loop_end)
 	{
 	  /* A possible loop end */
 
