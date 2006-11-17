1435bce3b21e343855022e936cc4a66961575d8e
Jie Zhang <jie.zhang@analog.com>
2006-11-17 09:40:42 +0000

    Remove.
    git-svn-id: svn://localhost/svn/toolchain/trunk@1285 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils/binutils-2.15/newlib/libc/machine/bfin/machine/endian.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MACHINE_ENDIAN_H
-# define _MACHINE_ENDIAN_H
-
-#define BYTE_ORDER LITTLE_ENDIAN
-
-#endif
--- binutils/binutils-2.15/newlib/libc/machine/bfin/machine/param.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MACHINE_PARAM_H
-# define _MACHINE_PARAM_H
-
-# define HZ (100)
-
-#define BYTE_ORDER LITTLE_ENDIAN
-
-#endif
