2b6d8e43efe3c7f0d4ba035b171392ede7885d61
Jie Zhang <jie.zhang@analog.com>
2007-02-12 04:22:01 +0000

    Task [#2403] 	* gcc.c-torture/execute/multi-ix.c: Disable the test if 	STACK_SIZE is too small. 	* gcc.c-torture/execute/20040313-1.c: Allow for small 	STACK_SIZE. 	* gcc.c-torture/execute/20040811-1.c: Likewise.
    git-svn-id: svn://localhost/svn/toolchain/trunk@1420 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/testsuite/gcc.c-torture/execute/20040313-1.c
+++ gcc-4.1/gcc/testsuite/gcc.c-torture/execute/20040313-1.c
@@ -1,17 +1,27 @@
 /* PR middle-end/14470 */
 /* Origin: Lodewijk Voge <lvoge@cs.vu.nl> */
 
+#ifdef STACK_SIZE
+#if STACK_SIZE < 5000
+#define NUM (STACK_SIZE / 5)
+#endif
+#endif
+
+#ifndef NUM
+#define NUM 1024
+#endif
+
 extern void abort(void);
 
 int main()
 {
-  int t[1025] = { 1024 }, d;
+  int t[NUM + 1] = { NUM }, d;
 
   d = 0;
   d = t[d]++;
-  if (t[0] != 1025)
+  if (t[0] != NUM + 1)
     abort();
-  if (d != 1024)
+  if (d != NUM)
     abort();
   return 0;
 }
