1a62b1b80bdff6ad1d60c70eaf897e535bf14373
Bernd Schmidt <bernds_cb1@t-online.de>
2007-06-14 13:29:57 +0000

    * configure.in: Don't add target-libmudflap to noconfigdirs for 	uclinux targets. 	* configure: Regenerate.
    	* gcc/config/bfin/uclinux.h (MFWRAP_SPEC): New.
    
    
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@1657 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/config/bfin/uclinux.h
+++ gcc-4.1/gcc/config/bfin/uclinux.h
@@ -7,3 +7,11 @@
 #define SUBTARGET_FDPIC_NOT_SUPPORTED
 
 #define MD_UNWIND_SUPPORT "config/bfin/linux-unwind.h"
+
+/* Like the definition in gcc.c, but for purposes of uClinux, every link is
+   static.  */
+#define MFWRAP_SPEC " %{fmudflap|fmudflapth: \
+ --wrap=malloc --wrap=free --wrap=calloc --wrap=realloc\
+ --wrap=mmap --wrap=munmap --wrap=alloca\
+ %{fmudflapth: --wrap=pthread_create\
+}} %{fmudflap|fmudflapth: --wrap=main}"
