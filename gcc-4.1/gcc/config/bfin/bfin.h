986088b8b1fe792d2a21667ac4f7a162d04313e3
Bernd Schmidt <bernds_cb1@t-online.de>
2007-09-06 22:04:13 +0000

    Bug[#3510] 	* config/bfin/bfin.h (PREFERRED_RELOAD_CLASS): Don't reload autoinc 	addresses into I registers.
    git-svn-id: svn://localhost/svn/toolchain/trunk@1796 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/config/bfin/bfin.h
+++ gcc-4.1/gcc/config/bfin/bfin.h
@@ -789,7 +789,10 @@ enum reg_class
    class to use when it is necessary to copy value X into a register
    in class CLASS.  The value is a register class; perhaps CLASS, or
    perhaps another, smaller class.  */
-#define PREFERRED_RELOAD_CLASS(X, CLASS) (CLASS)
+#define PREFERRED_RELOAD_CLASS(X, CLASS)		\
+  (GET_CODE (X) == POST_INC				\
+   || GET_CODE (X) == POST_DEC				\
+   || GET_CODE (X) == PRE_DEC ? PREGS : (CLASS))
 
 #define  SECONDARY_OUTPUT_RELOAD_CLASS(class,mode,x) \
     secondary_output_reload_class(class,mode,x)
