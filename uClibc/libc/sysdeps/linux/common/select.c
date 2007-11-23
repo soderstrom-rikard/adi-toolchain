9f1277d650113a132ba25293a2494d0e8c3c3b79
Bernd Schmidt <bernds_cb1@t-online.de>
2007-11-23 00:58:16 +0000

    Some tweaks to match the code that went upstream in r19847.
    git-svn-id: svn://localhost/svn/toolchain/trunk@1992 2adf4935-75e9-4407-93b2-34b21a1880a4

--- uClibc/libc/sysdeps/linux/common/select.c
+++ uClibc/libc/sysdeps/linux/common/select.c
@@ -12,33 +12,27 @@
 
 extern __typeof(select) __libc_select;
 
-#ifdef __NR_pselect6
+#if !defined(__NR__newselect) && !defined(__NR_select)
 
 # define __NR___libc_pselect6 __NR_pselect6
 _syscall6(int, __libc_pselect6, int, n, fd_set *, readfds, fd_set *, writefds,
-		fd_set *, exceptfds, const struct timespec *, timeout, 
-		const sigset_t *, sigmask);
+	fd_set *, exceptfds, const struct timespec *, timeout,
+	const sigset_t *, sigmask);
 
-int __libc_select (int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, 
-		struct timeval *timeout)
+int __libc_select(int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
+                  struct timeval *timeout)
 {
-	int result;
-	struct timespec ts;
-
-	if(timeout) {
-		ts.tv_sec = timeout->tv_sec;
-		ts.tv_nsec = timeout->tv_usec*1000;
-		result = __libc_pselect6(n, readfds, writefds, exceptfds, &ts, 0);
+	struct timespec _ts, *ts = 0;
+	if (timeout) {
+		_ts.tv_sec = timeout->tv_sec;
+		_ts.tv_nsec = timeout->tv_usec * 1000;
+		ts = &_ts;
 	}
-	else {
-		result = __libc_pselect6(n, readfds, writefds, exceptfds, 0, 0);
-	}
-	
-
-	return result;
+	return __libc_pselect6(n, readfds, writefds, exceptfds, ts, 0);
 }
 
 #else
+
 #ifdef __NR__newselect
 # define __NR___libc_select __NR__newselect
 #else
@@ -46,7 +40,9 @@ int __libc_select (int n, fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
 #endif
 _syscall5(int, __libc_select, int, n, fd_set *, readfds, fd_set *, writefds,
 		  fd_set *, exceptfds, struct timeval *, timeout);
+
 #endif
+
 libc_hidden_proto(select)
 weak_alias(__libc_select,select)
 libc_hidden_weak(select)
