de1f4ab66a240e112e6cef1bea5030d745111c58
Mike Frysinger <vapier@gentoo.org>
2008-05-06 01:19:11 +0000

    merge latest version from upstream
    git-svn-id: svn://localhost/svn/toolchain/trunk@2336 2adf4935-75e9-4407-93b2-34b21a1880a4

--- jtag/src/lib/fclock.c
+++ jtag/src/lib/fclock.c
@@ -26,9 +26,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#ifndef __MINGW32__
 #include <sys/times.h>
+#endif
 #include <math.h>
 #include <assert.h>
+
+/* ------------------------------------------------------------------ */
 
 #ifdef __APPLE__
 #include <mach/mach_time.h>
@@ -45,7 +49,7 @@ frealtime()
 
     mach_timebase_info_data_t mtid;
     struct timeval tv;
-
+       
     if(!mtid.denom == 0) {
         mach_timebase_info(&mtid);
         multiplier = (double)mtid.numer / (double)mtid.denom;
@@ -62,6 +66,54 @@ frealtime()
 }
 #else /* def __APPLE__ */
 
+/* ------------------------------------------------------------------ */
+
+#ifdef _POSIX_TIMERS
+
+long double
+frealtime()
+{
+    long double result;
+
+    struct timespec t;    
+    if (clock_gettime(CLOCK_REALTIME, &t)==-1) {
+        perror("frealtime (clock_gettime)");
+        exit(EXIT_FAILURE);
+    }
+    result = (long double)t.tv_sec + (long double)t.tv_nsec*(long double)1e-9;
+
+    assert(isnormal(result));
+    assert(result > 0);
+    return result;
+}
+
+#else /* def _POSIX_TIMERS */
+
+/* ------------------------------------------------------------------ */
+
+#ifdef __MINGW32__
+
+#include <sys/timeb.h>
+
+long double
+frealtime()
+{
+    long double result;
+
+    struct timeb t;
+
+    ftime(&t);
+    result = (long double)t.time + (long double)t.millitm * (long double)1e-3;
+
+    assert(isnormal(result));
+    assert(result > 0);
+    return result;
+}
+
+
+#else /* def __MINGW32__ */
+
+/* ------------------------------------------------------------------ */
 
 #ifndef CLK_TCK
 static clock_t CLK_TCK = 0;
@@ -77,19 +129,12 @@ static void set_clk_tck(void)
 }
 #endif
 
-
 long double
 frealtime()
 {
     long double result;
-#ifdef _POSIX_TIMERS
-    struct timespec t;    
-    if (clock_gettime(CLOCK_REALTIME, &t)==-1) {
-        perror("frealtime (clock_gettime)");
-        exit(EXIT_FAILURE);
-    }
-    result = (long double)t.tv_sec + (long double)t.tv_nsec*(long double)1e-9;
-#else
+
+
     struct tms t;
     clock_t c=times(&t);
     if (c==(clock_t)-1) {
@@ -97,23 +142,14 @@ frealtime()
         exit(EXIT_FAILURE);
     }
     result = (long double)c/CLK_TCK;
-#endif
+
     assert(isnormal(result));
     assert(result > 0);
     return result;
 }
 
-
-long double
-fcputime()
-{
-    struct tms t;
-    clock_t c=times(&t);
-    if (c==(clock_t)-1) {
-        perror("fcputime (times)");
-        exit(EXIT_FAILURE);
-    }
-    return ((long double)t.tms_utime+t.tms_stime)/CLK_TCK;
-}
-
 #endif
+#endif
+#endif
+
+
