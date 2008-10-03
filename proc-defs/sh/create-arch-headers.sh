28addc6f3a8067e0f4c69c2cca28a4e4c3f4ff65
Mike Frysinger <vapier@gentoo.org>
2008-10-03 00:15:51 +0000

    initial import of BF51x desc files
    git-svn-id: svn://localhost/svn/toolchain/trunk@2911 2adf4935-75e9-4407-93b2-34b21a1880a4

--- proc-defs/sh/create-arch-headers.sh
+++ proc-defs/sh/create-arch-headers.sh
@@ -37,6 +37,7 @@ echo "${common_header//@LOCAL@/CDEF}" > ${cdef_output}
 archdir() {
 	local p
 	case "$@" in
+	*51*)              p=bf518;;
 	*52*)              p=bf527;;
 	*531*|*532*|*533*) p=bf533;;
 	*534*|*536*|*537*) p=bf537;;
@@ -49,7 +50,7 @@ archdir() {
 }
 
 headers=""
-for bf in BF52{2,3,4,5,6,7} BF53{1,2,3,4,6,7,8,9} BF54{1,2,4,7,8,9} BF561 ; do
+for bf in BF51{2,4,6,8} BF52{2,3,4,5,6,7} BF53{1,2,3,4,6,7,8,9} BF54{1,2,4,7,8,9} BF561 ; do
 	xml="${srcdir}/xml/ADSP-${bf}-proc.xml"
 	arch=$(archdir ${xml})
 
