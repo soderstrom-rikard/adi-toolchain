e8a0731433181ad20d4385b621e76cc323d072b5
Jie Zhang <jie.zhang@analog.com>
2009-04-09 03:05:35 +0000

    Fix bug [#5058] gcc-4.3.3 ICEs with cmlt_fr16() 	* config/bfin/bfin.c (bfin_expand_builtin): Initialize icode 	before use. 	* config/bfin/bfin.md (AREG): Define mode iterator. 	(reload_in, reload_out): Use mode iterator AREG.
    	testsuite/
    	* gcc.target/bfin/20090409-1.c: New test.
    
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@3328 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.3/gcc/config/bfin/bfin.c
+++ gcc-4.3/gcc/config/bfin/bfin.c
@@ -6530,6 +6530,7 @@ bfin_expand_builtin (tree exp, rtx target ATTRIBUTE_UNUSED,
       op0 = expand_expr (arg0, NULL_RTX, VOIDmode, 0);
       op1 = expand_expr (arg1, NULL_RTX, VOIDmode, 0);
       accvec = gen_reg_rtx (V2PDImode);
+      icode = CODE_FOR_flag_macv2hi_parts;
 
       if (! target
 	  || GET_MODE (target) != V2HImode
