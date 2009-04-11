4f33fd5a0a5528fd593f04f2e1e8b92df1aee4e3
Jie Zhang <jie.zhang@analog.com>
2009-04-11 13:39:19 +0000

    [#5060] gcc 4.3.3 ICEs in df_uses_record, at df-scan.c:2977 with complex fract code 	* df.c (df_uses_record): Deal with VEC_SELECT. 	* df.h (enum df_ref_flags): Add DF_REF_VEC_SELECT.
    	testsuite/
    	* gcc.target/bfin/20090411-1.c: New test.
    
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@3336 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/df.h
+++ gcc-4.1/gcc/df.h
@@ -62,7 +62,10 @@ enum df_ref_flags
     /* This flag is set, if we stripped the subreg from the reference.
        In this case we must make conservative guesses, at what the
        outer mode was.  */
-    DF_REF_STRIPPED = 2
+    DF_REF_STRIPPED = 2,
+
+    /* This flag is set if the ref contains a VEC_SELECT.  */
+    DF_REF_VEC_SELECT = 4
   };
 
 
