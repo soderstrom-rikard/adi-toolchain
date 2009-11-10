ea927423bc1a83de5f1c5f620967a8b36e4c2d41
Mike Frysinger <vapier@gentoo.org>
2009-11-10 20:26:08 +0000

    initial import of tools to sign windows drivers
    git-svn-id: svn://localhost/svn/toolchain/trunk@3698 2adf4935-75e9-4407-93b2-34b21a1880a4

---
+++ win32/Prerequisites/driver-signing/inf2cat.bat
@@ -0,0 +1,3 @@
+set PATH=C:\WinDDK\7600.16385.0\bin\selfsign;%~dp0tools;%PATH%
+Inf2Cat.exe /driver:..\..\gnICE-drivers\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
+pause
---
+++ win32/Prerequisites/driver-signing/sign.bat
@@ -0,0 +1,4 @@
+set PATH=C:\WinDDK\7600.16385.0\bin\x86;%~dp0tools;%PATH%
+cd %~dp0..\..\gnICE-drivers
+signtool.exe sign /v /ac %~dp0\MSCV-VSClass3.cer /sha1 B61F9B648BB7B0A397809567880A4E90168A1678 /t http://timestamp.verisign.com/scripts/timstamp.dll * i386\* amd64\*
+pause
