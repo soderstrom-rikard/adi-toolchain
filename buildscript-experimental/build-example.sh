2eee5c29686c577cb6d2bce65835aed5370ff21e
Mike Frysinger <vapier@gentoo.org>
2010-03-15 07:34:59 +0000

    punt buildscript-experimental now that its ideas/work have been folded back into buildscript
    git-svn-id: svn://localhost/svn/toolchain/trunk@3999 2adf4935-75e9-4407-93b2-34b21a1880a4

--- buildscript-experimental/build-example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./toolchain-build -s /home/jie/blackfin-sources/toolchain/trunk/ -O /home/jie/installs/bfin-41-elf -b /home/jie/blackfin-sources/build41-elf -u /home/jie/blackfin-sources/u-boot/trunk/u-boot-2008.10/ -c 4.1 -t elf
-
-./toolchain-build -s /home/jie/blackfin-sources/toolchain/trunk/ -O /home/jie/installs/bfin-43-elf -b /home/jie/blackfin-sources/build43-elf -u /home/jie/blackfin-sources/u-boot/trunk/u-boot-2008.10/ -c 4.3 -t elf
-
--- buildscript-experimental/test-example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./toolchain-regtest -s /home/jie/blackfin-sources/toolchain/trunk -O /home/jie/installs/bfin-41-elf -b /home/jie/blackfin-sources/build41-elf-test-jtag -t elf-jtag -v -a
-
-./toolchain-regtest -s /home/jie/blackfin-sources/toolchain/trunk -O /home/jie/installs/bfin-43-elf -b /home/jie/blackfin-sources/build43-elf-test-jtag -t elf-jtag -v -a
