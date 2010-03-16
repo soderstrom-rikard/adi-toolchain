1c9fd52b7f04fc1d7daad3bd89f8a96248b02484
Mike Frysinger <vapier@gentoo.org>
2010-03-16 04:57:54 +0000

    sim: run sign test against A0 and A1
    git-svn-id: svn://localhost/svn/toolchain/trunk@4007 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.17/sim/testsuite/sim/bfin/sign.s
+++ binutils-2.17/sim/testsuite/sim/bfin/sign.s
@@ -5,19 +5,23 @@
 
 	start
 
-	.global _test
-
-	A0 = 0;
-	r0 = 0x10 (Z);
-	A0.x = R0;
+	.macro check_alu_signbits areg:req
+	\areg = 0;
+	R0 = 0x10 (Z);
+	\areg\().x = R0;
 
 	imm32 r0, 0x60038;
 
-	R0.L = SIGNBITS A0;
+	R0.L = SIGNBITS \areg;
 
 	imm32 r1, 0x6fffa;
-	CC = R1 == R0
-	if CC jump 1f;
-	fail
-1:
+	CC = R1 == R0;
+	if ! CC jump 1f;
+	.endm
+
+	check_alu_signbits A0
+	check_alu_signbits A1
+
 	pass
+1:
+	fail
