98783151c10c6beb751550bed0d3997ebafe3a68
Mike Frysinger <vapier@gentoo.org>
2010-03-29 23:13:25 +0000

    sim: use %s with printf strings to avoid gcc warnings
    git-svn-id: svn://localhost/svn/toolchain/trunk@4165 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.17/sim/common/sim-hw.c
+++ binutils-2.17/sim/common/sim-hw.c
@@ -258,7 +258,7 @@ hw_option_handler (struct sim_state *sd, sim_cpu *cpu, int opt,
 
     case OPTION_HW_DEVICE:
       {
-	hw_tree_parse (STATE_HW (sd)->tree, arg);
+	hw_tree_parse (STATE_HW (sd)->tree, "%s", arg);
 	return SIM_RC_OK;
       }
 
