a157f3e74f23aa86af11535fb62bac07575a34ef
Robin Getz <robin.getz@analog.com>
2010-04-05 16:20:49 +0000

    Fix the MAC issues with the TFU mode. Fixes two of the tests:  - c_dsp32mac_dr_a0_tu.s and  - c_dsp32mac_dr_a1_tu.s
    git-svn-id: svn://localhost/svn/toolchain/trunk@4279 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.17/sim/bfin/bfin-sim.c
+++ binutils-2.17/sim/bfin/bfin-sim.c
@@ -1470,6 +1470,11 @@ decode_macfunc (SIM_CPU *cpu, int which, int op, int h0, int h1, int src0,
 	    acc = 0x7fffffffffull, sat = 1;
 	  break;
 	case M_TFU:
+	  if (acc > 0xFFFFFFFFFFull)
+	    acc = 0x0, sat = 1;
+	  if (MM && acc > 0xFFFFFFFF)
+	    acc &= 0xFFFFFFFF;
+	  break;
 	case M_FU:
 	case M_IU:
 	  if (acc > 0xFFFFFFFFFFull)
