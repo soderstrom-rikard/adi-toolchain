bdddb3892b1fcd1cf1b7c381e5a45df7f5a40fdc
Mike Frysinger <vapier@gentoo.org>
2010-05-03 20:09:40 +0000

    sim: bfin: work around broken linux/ headers
    git-svn-id: svn://localhost/svn/toolchain/trunk@4553 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.17/sim/bfin/dv-eth_phy.c
+++ binutils-2.17/sim/bfin/dv-eth_phy.c
@@ -18,7 +18,11 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+/* Workaround old/broken linux headers.  */
+#define __ASSEMBLY__
+#define __u16 unsigned short
 #include <linux/mii.h>
+#undef __ASSEMBLY__
 
 #include "sim-main.h"
 #include "devices.h"
