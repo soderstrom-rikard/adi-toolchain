792275746879d4cdf9c9121288f6ee5151664bcb
Mike Frysinger <vapier@gentoo.org>
2010-08-25 20:40:49 +0000

    windows: update sha1 hash of new adi certs
    git-svn-id: svn://localhost/svn/toolchain/trunk@4839 2adf4935-75e9-4407-93b2-34b21a1880a4

--- distribution/windows/Prerequisites/driver-signing/sign.bat
+++ distribution/windows/Prerequisites/driver-signing/sign.bat
@@ -1,4 +1,4 @@
 set PATH=C:\WinDDK\7600.16385.0\bin\x86;%~dp0tools;%~dp0tools\x86;%PATH%
 cd %~dp0..\..\gnICE-drivers
-signtool.exe sign /v /ac %~dp0\MSCV-VSClass3.cer /sha1 B61F9B648BB7B0A397809567880A4E90168A1678 /t http://timestamp.verisign.com/scripts/timstamp.dll * i386\* amd64\*
+signtool.exe sign /v /ac %~dp0\MSCV-VSClass3.cer /sha1 BBEAD3B9B7BEC23098B752324E418884029FBDD9 /t http://timestamp.verisign.com/scripts/timstamp.dll * i386\* amd64\*
 pause
