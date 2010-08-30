fac71f983d99fd712a6208d8eabac198b73c6927
Mike Frysinger <vapier@gentoo.org>
2010-08-30 17:15:26 +0000

    libusb: update to 1.0.8
    git-svn-id: svn://localhost/svn/toolchain/trunk@4853 2adf4935-75e9-4407-93b2-34b21a1880a4

--- libusb/libusb/os/linux_usbfs.c
+++ libusb/libusb/os/linux_usbfs.c
@@ -72,14 +72,21 @@
 
 static const char *usbfs_path = NULL;
 
-/* Linux 2.6.32 adds support for a bulk continuation URB flag. this should
- * be set on all URBs in the transfer except the first. also set the
- * SHORT_NOT_OK flag on all of them, to raise error conditions on short
- * transfers.
- * then, on any error except a cancellation, all URBs until the next
- * non-continuation URB will be cancelled with the endpoint disabled,
- * meaning that no more data can creep in during the time it takes us to
- * cancel the remaining URBs. */
+/* Linux 2.6.32 adds support for a bulk continuation URB flag. this basically
+ * allows us to mark URBs as being part of a specific logical transfer when
+ * we submit them to the kernel. then, on any error error except a
+ * cancellation, all URBs within that transfer will be cancelled with the
+ * endpoint is disabled, meaning that no more data can creep in during the
+ * time it takes to cancel the remaining URBs.
+ *
+ * The BULK_CONTINUATION flag must be set on all URBs within a bulk transfer
+ * (in either direction) except the first.
+ * For IN transfers, we must also set SHORT_NOT_OK on all the URBs.
+ * For OUT transfers, SHORT_NOT_OK must not be set. The effective behaviour
+ * (where an OUT transfer does not complete, the rest of the URBs in the
+ * transfer get cancelled) is already in effect, and setting this flag is
+ * disallowed (a kernel with USB debugging enabled will reject such URBs).
+ */
 static int supports_flag_bulk_continuation = -1;
 
 /* clock ID for monotonic clock, as not all clock sources are available on all
@@ -381,7 +388,8 @@ static int sysfs_get_active_config(struct libusb_device *dev, int *config)
 
 /* takes a usbfs/descriptors fd seeked to the start of a configuration, and
  * seeks to the next one. */
-static int seek_to_next_config(struct libusb_context *ctx, int fd)
+static int seek_to_next_config(struct libusb_context *ctx, int fd,
+	int host_endian)
 {
 	struct libusb_config_descriptor config;
 	unsigned char tmp[6];
@@ -399,7 +407,7 @@ static int seek_to_next_config(struct libusb_context *ctx, int fd)
 	}
 
 	/* seek forward to end of config */
-	usbi_parse_descriptor(tmp, "bbwbb", &config, 1);
+	usbi_parse_descriptor(tmp, "bbwbb", &config, host_endian);
 	off = lseek(fd, config.wTotalLength - sizeof(tmp), SEEK_CUR);
 	if (off < 0) {
 		usbi_err(ctx, "seek failed ret=%d errno=%d", off, errno);
@@ -476,7 +484,7 @@ static int sysfs_get_active_config_descriptor(struct libusb_device *dev,
 		if (off < 0)
 			return LIBUSB_ERROR_IO;
 
-		r = seek_to_next_config(DEVICE_CTX(dev), fd);
+		r = seek_to_next_config(DEVICE_CTX(dev), fd, 1);
 		if (r < 0)
 			return r;
 	}
@@ -531,7 +539,7 @@ static int get_config_descriptor(struct libusb_context *ctx, int fd,
 	/* might need to skip some configuration descriptors to reach the
 	 * requested configuration */
 	while (config_index > 0) {
-		r = seek_to_next_config(ctx, fd);
+		r = seek_to_next_config(ctx, fd, 0);
 		if (r < 0)
 			return r;
 		config_index--;
@@ -606,7 +614,7 @@ static int cache_active_config(struct libusb_device *dev, int fd,
 		return r;
 	}
 
-	usbi_parse_descriptor(tmp, "bbw", &config, 1);
+	usbi_parse_descriptor(tmp, "bbw", &config, 0);
 	buf = malloc(config.wTotalLength);
 	if (!buf)
 		return LIBUSB_ERROR_NO_MEM;
@@ -645,7 +653,8 @@ static int usbfs_get_active_config(struct libusb_device *dev, int fd)
 		if (errno == ENODEV)
 			return LIBUSB_ERROR_NO_DEVICE;
 
-		usbi_err(DEVICE_CTX(dev),
+		/* we hit this error path frequently with buggy devices :( */
+		usbi_warn(DEVICE_CTX(dev),
 			"get_configuration failed ret=%d errno=%d", r, errno);
 		return LIBUSB_ERROR_IO;
 	}
@@ -715,7 +724,13 @@ static int initialize_device(struct libusb_device *dev, uint8_t busnum,
 				"determine active configuration descriptor", path);
 		} else {
 			active_config = usbfs_get_active_config(dev, fd);
-			if (active_config < 0) {
+			if (active_config == LIBUSB_ERROR_IO) {
+				/* buggy devices sometimes fail to report their active config.
+				 * assume unconfigured and continue the probing */
+				usbi_warn(DEVICE_CTX(dev), "couldn't query active "
+					"configuration, assumung unconfigured");
+				device_configured = 0;
+			} else if (active_config < 0) {
 				close(fd);
 				return active_config;
 			} else if (active_config == 0) {
@@ -724,7 +739,7 @@ static int initialize_device(struct libusb_device *dev, uint8_t busnum,
 				 * not support buggy devices in these circumstances.
 				 * stick to the specs: a configuration value of 0 means
 				 * unconfigured. */
-				usbi_dbg("assuming unconfigured device");
+				usbi_dbg("active cfg 0? assuming unconfigured device");
 				device_configured = 0;
 			}
 		}
@@ -851,10 +866,13 @@ static int usbfs_scan_busdir(struct libusb_context *ctx,
 			continue;
 		}
 
-		enumerate_device(ctx, &discdevs, busnum, (uint8_t) devaddr, NULL);
+		r = enumerate_device(ctx, &discdevs, busnum, (uint8_t) devaddr, NULL);
+		if (r < 0)
+			goto out;
 	}
 
 	*_discdevs = discdevs;
+out:
 	closedir(dir);
 	return r;
 }
@@ -1323,6 +1341,8 @@ static int submit_bulk_transfer(struct usbi_transfer *itransfer,
 	struct linux_device_handle_priv *dpriv =
 		__device_handle_priv(transfer->dev_handle);
 	struct usbfs_urb *urbs;
+	int is_out = (transfer->endpoint & LIBUSB_ENDPOINT_DIR_MASK)
+		== LIBUSB_ENDPOINT_OUT;
 	int r;
 	int i;
 	size_t alloc_size;
@@ -1361,7 +1381,7 @@ static int submit_bulk_transfer(struct usbi_transfer *itransfer,
 		urb->type = urb_type;
 		urb->endpoint = transfer->endpoint;
 		urb->buffer = transfer->buffer + (i * MAX_BULK_BUFFER_LENGTH);
-		if (supports_flag_bulk_continuation)
+		if (supports_flag_bulk_continuation && !is_out)
 			urb->flags = USBFS_URB_SHORT_NOT_OK;
 		if (i == num_urbs - 1 && last_urb_partial)
 			urb->buffer_length = transfer->length % MAX_BULK_BUFFER_LENGTH;
