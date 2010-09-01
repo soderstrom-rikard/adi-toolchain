92cb6d7097894ab54b90275b3bd4463c038f940c
Mike Frysinger <vapier@gentoo.org>
2010-09-01 18:12:39 +0000

    gcc-4.1: punt old version
    git-svn-id: svn://localhost/svn/toolchain/trunk@4874 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.1/gcc/testsuite/gcc.target/bfin/csync.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-final { scan-assembler "\[\t\]csync" } } */
-
-int main (void)
-{
-  __builtin_bfin_csync ();
-  return 0;
-}
--- gcc-4.1/gcc/testsuite/gcc.target/bfin/ssync.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-final { scan-assembler "\[\t\]ssync" } } */
-
-int main (void)
-{
-  __builtin_bfin_ssync ();
-  return 0;
-}
