a9f07eac7d31ec8c3679b09733f16ded599cba60
Norman Yee <norman.yee@analog.com>
2010-09-14 16:03:00 +0000

    - Replaced launch configuration type icon - Removed most classes from com.analog.gnu.toolchain.blackfin.debug.gdbjtag and related .gif files because they were copied from a slightly older gdbjtag plugin.  We now import those classes from the gdbjtag plugin.  GDBJTAGLaunchConfigurationDelegate was kept because they changed the call to debugger.createSession() so that null was passed to it instead of the program and debugging with the simulator does not work properly after this change. - Removed the Templates tab from the launch configuration dialog - Replaced functionality from Templates tab with 6 new launch configuration types:   - Blackfin Bare Metal Application (ELF) on JTAG Device   - Blackfin Bare Metal Application (ELF) on Simulator   - Blackfin Linux Kernel (ELF)   - Blackfin Linux Application (FLAT)   - Blackfin Linux Kernel (FLAT)   - Blackfin Linux Application (FDPIC)
    - Added launch shortcut for each new application launch configuration type.  Kernel types were omitted because the kernel is typically built from the command line via make and not through an Eclipse project.
    - Added Select Processor/rev page to project wizard.
    - Removed processor type list from plugin.xml and moved it to a separate file, GnuToolchainProcessor.xml, so that the processor list is stored in one place but used in two different dialog pages.
    - Removed the processor/rev selection from the Tool Settings tab under C/C++ Build->Settings and added them to a new Processor Settings tab
    
    
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@4905 2adf4935-75e9-4407-93b2-34b21a1880a4

--- debug-helpers/eclipse/com.analog.gnu.toolchain.blackfin/src/com/analog/gnu/toolchain/blackfin/debug/BlackfinDebugLaunchConfigurationTabGroup.java
+++ debug-helpers/eclipse/com.analog.gnu.toolchain.blackfin/src/com/analog/gnu/toolchain/blackfin/debug/BlackfinDebugLaunchConfigurationTabGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2009 Analog Devices, Inc.
+ *  Copyright (c) 2010 Analog Devices, Inc.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -11,22 +11,32 @@
 package com.analog.gnu.toolchain.blackfin.debug;
 
 import org.eclipse.cdt.launch.ui.CMainTab;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 
-import com.analog.gnu.toolchain.blackfin.debug.gdbjtag.GDBJtagDebuggerTab;
-import com.analog.gnu.toolchain.blackfin.debug.gdbjtag.GDBJtagStartupTab;
+import org.eclipse.cdt.debug.gdbjtag.ui.GDBJtagDebuggerTab;
+import org.eclipse.cdt.debug.gdbjtag.ui.GDBJtagStartupTab;
 
+import com.analog.gnu.toolchain.blackfin.Utility;
+
+/**
+ * Launch configuration tab group.
+ */
 public class BlackfinDebugLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
-
+	/**
+	 * Create the tabs in the launch configuration dialog.
+	 * 
+	 * @param dialog the launch configuration dialog
+	 * @param mode the launch configuration mode
+	 */
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		GDBJtagDebuggerTab debugger = new GDBJtagDebuggerTab();
 		GDBJtagStartupTab startup = new GDBJtagStartupTab();
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
-			new BlackfinDebugTemplates(debugger, startup),
 			new CMainTab(CMainTab.DONT_CHECK_PROGRAM),
 			debugger,
 			startup,
@@ -36,4 +46,18 @@ public class BlackfinDebugLaunchConfigurationTabGroup extends AbstractLaunchConf
 		setTabs(tabs);
 	}
 
+	/**
+	 * setDefaults is called when the user creates a new launch configuration.
+	 * This function sets up the default settings for the tabs in the GDB JTAG
+	 * debugger plugin, based on the launch configuration type selected by the
+	 * user.  For example, if the user chooses Blackfin Linux App (FLAT), then
+	 * the gdb command will be set to bfin-linux-gdb.
+	 * 
+	 * @param configuration - the launch configuration
+	 */
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+		super.setDefaults(configuration);
+		  
+		Utility.initDefaults(configuration);
+	}
 }
