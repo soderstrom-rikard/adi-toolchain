0f731345d2b9db176f698c0ee23bb83cb6e4cdac
Steve Kilbane <steve@whitecrow.demon.co.uk>
2010-09-20 14:10:43 +0000

    [#5827] uClibc - Clean up previous checkin for #5827.
    cacheflush.c: Add copyright comment, header. Correct decl types.
    sram-free.c: Include header, correct decl types.
    sram-alloc.c: Include header, correct decl types.
    dma-memcpy.c: Include header, correct decl types.
    cachectl.h: Revise copyright comment, punt old comment, remove __THROW.
    
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@4921 2adf4935-75e9-4407-93b2-34b21a1880a4

--- uClibc/libc/sysdeps/linux/bfin/sys/cachectl.h
+++ uClibc/libc/sysdeps/linux/bfin/sys/cachectl.h
@@ -1,32 +1,13 @@
-/* Copyright (C) 2010 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
+/*
+ * cachectl.h - Functions for cache control on Blackfin.
+ *
+ * Copyright (C) 2010 Analog Devices, Inc.
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
 
 #ifndef _SYS_CACHECTL_H
 #define _SYS_CACHECTL_H	1
-
-/* This is modelled after the file of the same name on MIPS
-   systems.  It provides a definition of the core file format for ELF
-   used on Linux.  It doesn't have anything to do with the /proc file
-   system, even though Linux has one.
-
-   Anyway, the whole purpose of this file is for GDB and GDB only.
-   Don't read too much into it.  Don't use it for anything other than
-   GDB unless you know what you are doing.  */
 
 #include <features.h>
 
@@ -37,7 +18,7 @@
 
 __BEGIN_DECLS
 
-extern int cacheflush (void *addr, __const int nbytes, __const int flags) __THROW;
+extern int cacheflush (void *addr, __const int nbytes, __const int flags);
 
 __END_DECLS
 
