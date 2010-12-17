281f2c018786cc78a08915f2b404c501597ef778
Mike Frysinger <vapier@gentoo.org>
2010-12-17 01:54:28 +0000

    newlib: bfin: forward port clear_cache_range/exit hacks
    these need to be redone slightly in conjunction with gcc spec updates,
    but for now this gets us back to feature parity with our older newlib
    version with binutils-2.17
    
    git-svn-id: svn://localhost/svn/toolchain/trunk@5048 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.21/libgloss/libnosys/_exit.c
+++ binutils-2.21/libgloss/libnosys/_exit.c
@@ -9,9 +9,14 @@ _VOID
 _DEFUN (_exit, (rc),
 	int rc)
 {
+#ifdef __BFIN__
+  asm ("EXCPT 0;");
+#else
   /* Default stub just causes a divide by 0 exception.  */
   int x = rc / INT_MAX;
   x = 4 / x;
+  asm volatile ("" : : "r" (x));
+#endif
 
   /* Convince GCC that this function never returns.  */
   for (;;)
