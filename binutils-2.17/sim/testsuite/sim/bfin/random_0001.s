1cebb85069d25690347b2b34ca1d5d32dcf942e5
Mike Frysinger <vapier@gentoo.org>
2011-03-14 23:05:16 +0000

    sim: tests: redo tests with dmm32 and add comments
    git-svn-id: svn://localhost/svn/toolchain/trunk@5316 2adf4935-75e9-4407-93b2-34b21a1880a4

--- binutils-2.17/sim/testsuite/sim/bfin/random_0001.s
+++ binutils-2.17/sim/testsuite/sim/bfin/random_0001.s
@@ -1,10 +1,12 @@
+# Test for saturation behavior with fract multiplication
 # mach: bfin
+
 .include "testutils.inc"
 
 	start
 
-	imm32 R0, 0x45c1969f;   A0.w = R0;
-	imm32 R0, 0x00000000;   A0.x = R0;
+	dmm32 A0.w, 0x45c1969f;
+	dmm32 A0.x, 0x00000000;
 	R4 = A0 (IU);
 	checkreg R4, 0x45c1969f;
 
