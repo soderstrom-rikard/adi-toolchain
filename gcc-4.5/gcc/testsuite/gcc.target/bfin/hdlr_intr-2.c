1503346408fd3ec6973d3bc47ee0bb151c026a81
Stuart Henderson <stuart.henderson@analog.com>
2011-06-17 11:02:19 +0000

    [#6092] gcc-4.5/gcc/testsuite: RETI would only match pop, not push
    git-svn-id: svn://localhost/svn/toolchain/trunk@5519 2adf4935-75e9-4407-93b2-34b21a1880a4

--- gcc-4.5/gcc/testsuite/gcc.target/bfin/hdlr_intr-2.c
+++ gcc-4.5/gcc/testsuite/gcc.target/bfin/hdlr_intr-2.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-final { scan-assembler "\[\t\]rti" } } */
-/* { dg-final { scan-assembler-times "\[\t\]RETI" 2 } } */
+/* { dg-final { scan-assembler-times "RETI" 2 } } */
 /* { dg-final { scan-assembler-not "\[\t\]rts" } } */
 /* { dg-final { scan-assembler-not "\[\t\]rtn" } } */
 /* { dg-final { scan-assembler-not "\[\t\]rtx" } } */
